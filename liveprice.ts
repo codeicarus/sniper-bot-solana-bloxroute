3da6bc576534785e4d27d12ca88752f31dc3805c
codeicarus <esoph.dev.9@gmail.com>
2024-08-31 11:50:34 +0900

    initial commit

---
+++ liveprice.ts
@@ -0,0 +1,39 @@
+import { Connection, PublicKey } from '@solana/web3.js';
+import axios from 'axios';
+import mintAddresses from "./mintaddress.json"
+
+// Define the connection to the Solana network
+const connection = new Connection('https://api.mainnet-beta.solana.com');
+
+// Function to get the live price of a token given its mint address
+async function getTokenLivePrice(symbol:string, mintAddress: string) {
+  try {
+    const response = await axios.get(`https://api-v3.raydium.io/mint/price?mints=${mintAddress}`, {
+      headers: {
+        'accept': 'application/json'
+      }
+    });
+    const priceData = response.data.data[mintAddress];
+    if (!priceData) {
+      console.error('Price data not found for the token mint address:', symbol);
+      return null;
+    }
+
+    const price = priceData;
+    console.log(`${symbol} live price: $${priceData}`);
+    return price;
+  } catch (error) {
+    console.error('Error fetching token price:', error);
+    return null;
+  }
+}
+
+// Optional: Set an interval to fetch the price continuously
+setInterval(() => {
+  console.log("\n")
+  console.log(new Date().toLocaleString(), ": Fetching Token Prices...")
+  Object.keys(mintAddresses).map(async (key) => {
+    await getTokenLivePrice(key, mintAddresses[key]);
+  })
+  console.log("=====================================================")
+}, 3000);
